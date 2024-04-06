59323b4b19596ab581620131dc57a288c3e10878
Gabriel de Oliveira <gabrielhk3@gmail.com>
2024-04-05 21:22:25 -0300

    fix daysRemainingMessage

--- src/dates/daysRemainingMessage.ts
+++ src/dates/daysRemainingMessage.ts
@@ -1,8 +1,8 @@
 export default function daysRemainingMessage(days: number) {
   if (days === 0) {
     return "Hoje é o quinto dia útil do mês!";
-  } else if (days === 0) {
-    return "O próximo quinto dia útil do mês é amanhã!";
+  } else if (days === 1) {
+    return "Amanhã é o próximo quinto dia útil do mês!";
   } else {
     return `Faltam ${days} dias até o próximo quinto dia útil.`;
   }
