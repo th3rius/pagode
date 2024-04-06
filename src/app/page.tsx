1df95fb304edbf79d8930e05c88ed95c5f975c75
Gabriel de Oliveira <gabrielhk3@gmail.com>
2024-04-06 13:31:44 -0300

    update github buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import { getNextFifthBusinessDay } from "@/dates/payday";
 import countDaysInBetween from "@/dates/countDaysInBetween";
 import daysRemainingMessage from "@/dates/daysRemainingMessage";
-import GitHubButtonClient from "@/components/GitHubButtonClient";
+import Script from "next/script";
 
 export default function Home() {
   const today = new Date();
@@ -11,8 +11,10 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
+      <Script async defer src="https://buttons.github.io/buttons.js" />
       <div className={styles.github}>
-        <GitHubButtonClient
+        <a
+          className="github-button"
           href="https://github.com/th3rius/pagode"
           data-color-scheme="no-preference: light; light: light; dark: dark;"
           data-icon="octicon-star"
@@ -21,15 +23,16 @@ export default function Home() {
           aria-label="Favoritar th3rius/pagode em GitHub"
         >
           Favoritar
-        </GitHubButtonClient>
-        <GitHubButtonClient
+        </a>
+        <a
+          className="github-button"
           href="https://github.com/th3rius"
           data-color-scheme="no-preference: light; light: light; dark: dark;"
           data-size="large"
           aria-label="Seguir @th3rius em GitHub"
         >
           Seguir @th3rius
-        </GitHubButtonClient>
+        </a>
       </div>
 
       <div className={styles.center}>
