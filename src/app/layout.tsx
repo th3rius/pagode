64f449f46ea2a6ce2c36892fbea3fdd6cf27b919
Gabriel de Oliveira <gabrielhk3@gmail.com>
2024-04-05 21:18:02 -0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "@fontsource/iosevka-aile";
 import "./globals.css";
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
